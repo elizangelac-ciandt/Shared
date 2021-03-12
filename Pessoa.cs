e83c509b0097306fd3c5abed27a20a2c9c6fb34f
Elizangela Calegari <elizangelac@ciandt.com>
2021-03-12 14:42:04 -0300

    First commit

---
+++ Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SharedIngest
+{
+    public static class Pessoa
+    {
+        public static string CalcularIdade(DateTime dataNascimento)
+        {
+            var idade = DateTime.Now.Year - dataNascimento.Year;
+            if(DateTime.Now.DayOfYear < dataNascimento.DayOfYear)
+                idade = idade - 1;
+
+            return idade.ToString();
+        }
+    }
+}
