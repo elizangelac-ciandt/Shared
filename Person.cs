39c197415c1dfb01e75c897b2fd1eb28c1335ff0
Elizangela Calegari <elizangelac@ciandt.com>
2021-03-15 17:11:18 -0300

    Fix idiom

---
+++ Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SharedIngest
+{
+    public static class Person
+    {
+        public static string CalculateAge(DateTime birthDate)
+        {
+            var age = DateTime.Now.Year - birthDate.Year;
+            if(DateTime.Now.DayOfYear < birthDate.DayOfYear)
+                age = age - 1;
+
+            return age.ToString() + "  RELEASE";
+        }
+    }
+}
